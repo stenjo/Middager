3da38d5e796ae35e848568e905a1eafc22e9983f
Sten Johnsen <sten.johnsen@bouvet.no>
2023-11-07 19:52:15 +0100

    fix(dateHandling): trouble with dates from appointment

--- tests/dateHandling_test.py
+++ tests/dateHandling_test.py
@@ -1,20 +1,19 @@
-import datetime
-
+from datetime import datetime, timezone
 from dateHandling import dayText, isNowInTimePeriod
 
 def test_isNowInTimePeriod():
-    startTime = datetime.datetime.strptime("2023-10-22T10:12", "%Y-%m-%dT%H:%M")
-    endTime = datetime.datetime.strptime("2023-10-25T10:12", "%Y-%m-%dT%H:%M")
-    today = datetime.datetime.strptime("2023-10-23T10:12", "%Y-%m-%dT%H:%M")
-    before = datetime.datetime.strptime("2023-10-22T10:11", "%Y-%m-%dT%H:%M")
-    after = datetime.datetime.strptime("2023-10-25T10:13", "%Y-%m-%dT%H:%M")
+    startTime = datetime.strptime("2023-10-22T10:12", "%Y-%m-%dT%H:%M")
+    endTime = datetime.strptime("2023-10-25T10:12", "%Y-%m-%dT%H:%M")
+    today = datetime.strptime("2023-10-23T10:12", "%Y-%m-%dT%H:%M")
+    before = datetime.strptime("2023-10-22T10:11", "%Y-%m-%dT%H:%M")
+    after = datetime.strptime("2023-10-25T10:13", "%Y-%m-%dT%H:%M")
     
     assert isNowInTimePeriod(startTime, endTime, today) == True
     assert isNowInTimePeriod(startTime, endTime, before) == False
     assert isNowInTimePeriod(startTime, endTime, after) == False
 
 
-today = datetime.datetime.strptime("2023-10-23T10:12", "%Y-%m-%dT%H:%M")
+today = datetime.strptime("2023-10-23T10:12", "%Y-%m-%dT%H:%M").replace(tzinfo=timezone.utc)
     
 def test_dayText_tomorrow():
     event = {
@@ -75,7 +74,7 @@ def test_dayText_today_with_time_and_timezone():
         },
         'summary': 'Event summary'
     }
-    today = datetime.datetime.strptime("2023-10-23T10:12 +0100", "%Y-%m-%dT%H:%M %z")
+    today = datetime.strptime("2023-10-23T10:12 +0100", "%Y-%m-%dT%H:%M %z")
     # print(today)
     # dt:  2023-11-09 13:15:00+01:00
     # today:  2023-11-04 18:54:37.512734
@@ -85,11 +84,21 @@ def test_dayText_today_with_time_and_timezone():
 def test_dayText_today_with_datetime_and_timezone():
     event = {
         'start': {
-            'date': datetime.datetime.today().strftime('%Y-%m-%d'),
-            'dateTime': datetime.datetime.today().strftime('%Y-%m-%d') + 'T10:22:18+01:00'
+            'date': datetime.today().strftime('%Y-%m-%d'),
+            'dateTime': datetime.today().strftime('%Y-%m-%d') + 'T10:22:18+01:00'
         },
         'summary': 'Event summary'
     }
 
     assert dayText(event) == "I dag: Event summary kl. 10:22"
-    
+
+def test_dayText_today_with_date_and_no_timezone():
+    event = {
+        'start': {
+            'date': datetime.today().strftime('%Y-%m-%d'),
+        },
+        'summary': 'Event summary'
+    }
+
+    assert dayText(event) == "I dag: Event summary"
+
